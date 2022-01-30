990fc34880c8509a53124f7c1863b2bdf63f1f9b
Betselot-GA <betselotgetnet2227@gmail.com>
2022-01-30 11:36:05 +0300

    fixed connection

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { ContextProvider } from "./context/Context";
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:4000/';
+axios.defaults.baseURL = 'http://localhost:4000/api';
 // axios.defaults.baseURL = 'http://localhost:4000/api';
 
 ReactDOM.render(
